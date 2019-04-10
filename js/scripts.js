33414673b3a6c321f247f57777ea2d494161c01e
Jon <roomcleaner@protonmail.org>
2019-04-09 23:54:46 -0500

    Fix parallax on mobile, add form/nav ELs

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,34 @@ function shiftBackground() {
     // implements parallax effects
     let fromTop = window.pageYOffset;
     let fromBot = body.scrollHeight - (fromTop + window.innerHeight);
-    topBG.style.backgroundPosition = "center bottom " + (fromTop * 0.5) + "px";
-    botBG.style.backgroundPosition = "center top " + (fromBot * 0.5) + "px";
+    topBG.style.backgroundPosition = "center bottom " + (-fromTop * 0.5) + "px";
+    botBG.style.backgroundPosition = "center top " + (-fromBot * 0.5) + "px";
 }
 
 window.addEventListener('scroll', () => {
     requestAnimationFrame(() => shiftBackground());
 }, false);
+
+function addNav() {
+    document.querySelector(".global-nav.fixed").style.transform = "translate(0,0rem)";
+}
+
+function remNav() {
+    document.querySelector(".global-nav.fixed").style.transform = "translate(0,-3rem)";
+}
+
+document.querySelectorAll('.gform>.form-text-field').forEach((field) => {
+    field.addEventListener('blur', () => {
+        if (field.value) {
+            field.classList.add('filled');
+        } else {
+            field.classList.remove('filled');
+        }
+    });
+});
+
+document.querySelector('.gform>input[type=reset]').addEventListener('click', () => {
+    document.querySelectorAll('.gform>.form-text-field').forEach((field) => field.classList.remove('filled'));
+});
+
+let observer = new IntersectionObserver();
