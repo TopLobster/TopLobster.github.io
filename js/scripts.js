d0de3f7ee4acee9906bebc4524d3591d40d21b48
Jon <roomcleaner@protonmail.org>
2019-04-14 14:18:55 -0500

    Refactor to implement CSS parallax method

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,14 @@
 "use strict";
 
-/* animate backgrounds on scroll */
-// TODO refactor to make this use Intersection Observers on each image for better performance
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function(f){setTimeout(f, 1000/60);};
-
-const topBG = document.querySelector('#banner>.background');
-const botBG = document.querySelector('#contact>.background');
-const body = document.querySelector('body');
-
-function shiftBackground() {
-    // implements parallax effects
-    let fromTop = window.pageYOffset;
-    let fromBot = body.scrollHeight - (fromTop + window.innerHeight);
-    topBG.style.transform = 'translate(0,' + (fromTop * 0.7) + "px)";
-    botBG.style.transform = 'translate(0,' + (-fromBot * 0.7) + "px)";
+/* Add and remove the Nav on scroll */
+function addtdv() {
+    document.querySelectorAll("section").forEach(section => section.classList.add('tdv'));
 }
 
-window.addEventListener('scroll', () => {
-    requestAnimationFrame(() => shiftBackground());
-}, false);
+function remtdv() {
+    document.querySelectorAll("section").forEach(section => section.classList.remove('tdv'));
+}
 
-/* Add and remove the Nav on scroll */
 function addNav() {
     document.querySelector(".global-nav.fixed").style.transform = "translate(0,0rem)";
 }
